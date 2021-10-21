a84736c8aafe2a1bd7bef6580eca2fc7ff5fe937
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-10-20 19:18:47 -0500

    update

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const session = require("express-session");
-const sequelize = require("../../config/connection");
+// const session = require("express-session");
+// const sequelize = require("../../config/connection");
 const { Post, User, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
